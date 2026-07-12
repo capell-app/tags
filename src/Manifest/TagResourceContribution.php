c75a328bc0bf20b5cdc368d6ec4af935af1f86d3
Ben <howdu@users.noreply.github.com>
2026-07-12 08:56:25 +0100

    fix: align packages with Capell v4 extension API

--- src/Manifest/TagResourceContribution.php
+++ src/Manifest/TagResourceContribution.php
@@ -11,6 +11,6 @@ final class TagResourceContribution implements ExtensionContribution, RegistersE
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^0.0';
+        return '^4.0';
     }
 }
