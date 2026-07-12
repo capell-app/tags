92492fecc9817d71c0c2f0db640615b46c80dba3
Ben <howdu@users.noreply.github.com>
2026-07-12 07:44:22 +0100

    fix: align packages with 0.0 release line

--- src/Manifest/TagsModelsContribution.php
+++ src/Manifest/TagsModelsContribution.php
@@ -10,6 +10,6 @@ final class TagsModelsContribution implements ExtensionContribution
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 }
